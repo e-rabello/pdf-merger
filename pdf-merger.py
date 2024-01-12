aac3337df96d98fd2bed14e3f5f01f70f3420fe7
e-rabello <8457978+e-rabello@users.noreply.github.com>
2024-01-12 09:39:48 -0300

    Update pdf-merger.py

--- pdf-merger.py
+++ pdf-merger.py
@@ -1,11 +1,13 @@
 from pypdf import PdfMerger
 
-pdfs = ['document-page0.pdf', 'document-page1.pdf']
+# add as many files as you want
+pdfs = ['document-page0.pdf', 'document-page1.pdf', 'document-page2.pdf']
 
 merger = PdfMerger()
 
 for pdf in pdfs:
     merger.append(pdf)
 
+# resulting file
 merger.write("result.pdf")
 merger.close()
